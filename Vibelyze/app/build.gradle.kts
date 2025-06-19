e8104b5655066ec272103a9943095cee696caa08
DanielRivera237 <00048722@uca.edu,sv>
2025-06-19 14:53:49 -0600

    Ajustes

--- Vibelyze/app/build.gradle.kts
+++ Vibelyze/app/build.gradle.kts
@@ -58,6 +58,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.accompanist.pager)
     implementation(libs.accompanist.pager.indicators)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("androidx.compose.material:material-icons-extended")
 }
